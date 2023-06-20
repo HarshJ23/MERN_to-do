f36fc0630b61ea750cc2c8d983d0fe0c5e0bea0d
HarshJ23 <hjawajiwar@gmail.com>
2023-06-20 16:49:54 +0530

    Connected to Atlas

--- data/database.js
+++ data/database.js
@@ -5,5 +5,5 @@ export const connectDB =()=>{
 
     mongoose.connect(process.env.MONGO_URI, {
         dbName:"backendAPI",  
-    }).then(()=>console.log("Database connected")).catch((e)=>console.log(e));
+    }).then((c)=>console.log(`Database connected with ${c.connection.host}`)).catch((e)=>console.log(e));
 }
